1cc966d966926aa0d6ce09e11020a93ae8bef517
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-07-12 00:19:48 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { Provider } from "react-redux";
+// import { store, persistor } from "./redux/store";
 import "./index.css";
 import App from "./components/App/App";
 
