b1c83088a7fde058a9aea1960f7d66c8b782fc8b
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-07-12 00:56:58 +0300

    upd

--- src/components/App/App.jsx
+++
@@ -1,9 +0,0 @@
-// import "./App.css";
-
-function App() {
-  return <>
-    <div>This is APp</div>
-  </>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
 
-import { BrowserRouter } from "react-router-dom";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { Provider } from "react-redux";
-// import { store, persistor } from "./redux/store";
-import "./index.css";
-import App from "./components/App/App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      {/* </PersistGate> */}
-    {/* </Provider> */}
+    <App />
   </React.StrictMode>
 );
-
